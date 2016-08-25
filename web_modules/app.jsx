e1010fec24d96442accf5e8287727173bc9a7055
Jesse Clark <jjclark1982@gmail.com>
2016-08-25 10:22:31 +1000

    link to github issues

--- web_modules/app.jsx
+++ web_modules/app.jsx
@@ -26,12 +26,18 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
+                <div style={{position:'absolute', right: '1em', top: '1em'}}>
+                    <a className="github-button" href="https://github.com/jjclark1982/no-mans-sky-simulator/issues" data-style="mega" data-count-api="/repos/jjclark1982/no-mans-sky-simulator#open_issues_count" data-count-aria-label="# issues on GitHub" aria-label="Issue jjclark1982/no-mans-sky-simulator on GitHub">Issue</a>
+                </div>
                 <h1><i>No Man’s Sky</i> Simulator</h1>
                 <label>
                     Seed: <input type="text" name="seed" value={this.state.seed} onChange={this.setSeed} />
                 </label>
                 <button type="button" onClick={this.randomizeSeed}>Randomize</button>
                 <Colorizer src="images/ss_1.1920x1080.jpg" seed={this.state.seed}/>
+                <footer>
+                    version {process.env.PACKAGE_VERSION}
+                </footer>
             </div>
         );
     }
